cbaeb9cc30c24d233bc562c5fde53f7134397180
Sierra Wallace <sskwallace@gmail.com>
2020-09-26 02:42:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       end
     end
     def reverse_transfer
-      if self.status = "complete"
+      if self.status == "complete"
         self.sender.balance += amount
         self.receiver.balance -= amount
         self.status = "reversed"
